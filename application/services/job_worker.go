2bb28cdbc3a0fedf1a09629fd82a0501883d5a77
Andreis3 <andrei.as3@hotmail.com>
2022-01-17 19:24:26 -0300

    feat: implements job_worker

--- application/services/job_worker.go
+++ application/services/job_worker.go
@@ -1,9 +1,13 @@
 package services
 
 import (
+	"encoding/json"
 	"github.com/Andreis3/encoder-video-golang/domain"
 	"github.com/Andreis3/encoder-video-golang/framework/utils"
+	uuid "github.com/satori/go.uuid"
 	"github.com/streadway/amqp"
+	"os"
+	"time"
 )
 
 type JobWorkerResult struct {
@@ -12,7 +16,7 @@ type JobWorkerResult struct {
 	Error   error
 }
 
-func JobWorker(messageChanel chan amqp.Delivery, returnChan chan JobWorkerResult, jobService JobService, workerID int) {
+func JobWorker(messageChanel chan amqp.Delivery, returnChan chan JobWorkerResult, jobService JobService, job domain.Job, workerID int) {
 	// pega msg de body do json
 	// validar se o json é um json. Se é valido o json
 	// validar o video
@@ -22,7 +26,48 @@ func JobWorker(messageChanel chan amqp.Delivery, returnChan chan JobWorkerResult
 		err := utils.IsJson(string(message.Body))
 		if err != nil {
 			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
 		}
+
+		err = json.Unmarshal(message.Body, &jobService.VideoService.Video)
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+		jobService.VideoService.Video.ID = uuid.NewV4().String()
+
+		err = jobService.VideoService.Video.Validate()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.InsertVideo()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		job.Video = jobService.VideoService.Video
+		job.OutputBucketPath = os.Getenv("outputBucketName")
+		job.ID = uuid.NewV4().String()
+		job.Status = "STARTING"
+		job.CreatedAt = time.Now()
+
+		_, err = jobService.JobRepository.Insert(&job)
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		jobService.Job = &job
+		err = jobService.Start()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		returnChan <- returnJobResult(job, message, nil)
 	}
 }
 
